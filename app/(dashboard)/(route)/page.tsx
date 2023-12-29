24171662f52540615bbad3854cf6c5c683299556
Aniruddha <das.aniruddha4321@gmail.com>
2023-12-29 17:12:46 +0530

    chore:Auth added

--- app/(dashboard)/(route)/page.tsx
+++ app/(dashboard)/(route)/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@/components/ui/Button'
+import { UserButton } from "@clerk/nextjs";
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <p>This is a protected page</p>
+    <div className="h-screen">
+      <UserButton afterSignOutUrl="/"/>
     </div>
   )
 }
